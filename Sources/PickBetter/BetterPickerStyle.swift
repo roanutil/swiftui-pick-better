61f98461a67805dd04ccef7d0dffdaea1da9d642
Ryan Jarvis <ryan@ryans-laptop.lan>
2024-11-08 09:58:40 -0600

    Update header dates via swiftformat

--- Sources/PickBetter/BetterPickerStyle.swift
+++ Sources/PickBetter/BetterPickerStyle.swift
@@ -1,7 +1,7 @@
 // BetterPickerStyle.swift
 // PickBetter
 //
-// Copyright © 2023 MFB Technologies, Inc. All rights reserved. All rights reserved.
+// Copyright © 2024 MFB Technologies, Inc. All rights reserved. All rights reserved.
 //
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
